2c0353b0b43a851710ca97681e17660bb578adfa
Quinn Keaveney <qkeave@gmail.com>
2026-04-07 11:08:22 -0700

    Rename package to @liiift-studio/gray-value

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
 				<div className="flex flex-col gap-8 text-sm">
 					<div className="flex flex-col gap-3">
 						<p className="opacity-50">Drop-in component</p>
-						<CodeBlock code={`import { GrayValueText } from 'gray-value'
+						<CodeBlock code={`import { GrayValueText } from '@liiift-studio/gray-value'
 
 <GrayValueText maxAdjustment={0.05} calibrationFactor={2}>
   Your paragraph text here...
--- site/src/components/CopyInstall.tsx
+++ site/src/components/CopyInstall.tsx
@@ -3,7 +3,7 @@
 // Click-to-copy npm install snippet with a link to the npm package page
 import { useState } from "react"
 
-const CMD = "npm install gray-value"
+const CMD = "npm install @liiift-studio/gray-value"
 const NPM_URL = "https://www.npmjs.com/package/gray-value"
 
 /** Displays the install command, copies it to clipboard on click, and links to npm */
