0d437d56f3571414d422839880d0412d3dcbc23d
Quinn Keaveney <qkeave@gmail.com>
2026-04-07 17:08:06 -0700

    Rename to camelCase: @liiift-studio/steadyGray

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
 				<div className="flex flex-col gap-8 text-sm">
 					<div className="flex flex-col gap-3">
 						<p className="opacity-50">Drop-in component</p>
-						<CodeBlock code={`import { GrayValueText } from '@liiift-studio/steady-gray'
+						<CodeBlock code={`import { GrayValueText } from '@liiift-studio/steadyGray'
 
 <GrayValueText maxAdjustment={0.05} calibrationFactor={2}>
   Your paragraph text here...
--- site/src/components/CopyInstall.tsx
+++ site/src/components/CopyInstall.tsx
@@ -3,7 +3,7 @@
 // Click-to-copy npm install snippet with a link to the npm package page
 import { useState } from "react"
 
-const CMD = "npm install @liiift-studio/steady-gray"
+const CMD = "npm install @liiift-studio/steadyGray"
 const NPM_URL = "https://www.npmjs.com/package/gray-value"
 
 /** Displays the install command, copies it to clipboard on click, and links to npm */
