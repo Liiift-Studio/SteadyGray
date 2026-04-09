96e1c796f2a146488c0f4ea54700ac2a49c4dab3
Quinn Keaveney <qkeave@gmail.com>
2026-04-09 00:26:39 -0700

    Footer: replace Liiift Studio text link with logo image

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -106,7 +106,7 @@ removeGrayValue(el, original)`} />
 				<ToolDirectory current="steadyGray" />
 				<div className="flex justify-between opacity-50">
 				<span>steadyGray v{version}</span>
-				<a href="https://liiift.studio" className="hover:opacity-100 transition-opacity">Liiift Studio</a>
+				<a href="https://liiift.studio" className="hover:opacity-100 transition-opacity"><img src="/logo-white.png" alt="Liiift Studio" style={{ height: 18, display: 'block' }} /></a>
 				</div>
 			</footer>
 
