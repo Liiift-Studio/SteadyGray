cb1d484519d92120a9fd4f9ef5b84ce6fa0732a1
Quinn Keaveney <qkeave@gmail.com>
2026-04-09 13:19:08 -0700

    Fix npm URL in CopyInstall

--- site/src/components/CopyInstall.tsx
+++ site/src/components/CopyInstall.tsx
@@ -4,7 +4,7 @@
 import { useState } from "react"
 
 const CMD = "npm install @liiift-studio/steadygray"
-const NPM_URL = "https://www.npmjs.com/package/gray-value"
+const NPM_URL = "https://www.npmjs.com/package/@liiift-studio/steadygray"
 
 /** Displays the install command, copies it to clipboard on click, and links to npm */
 export default function CopyInstall() {
